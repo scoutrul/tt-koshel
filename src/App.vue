9de0c91cee454213cbdc0323fb8e238cb41c3aea
Anton G <antongolova@gmail.com>
2025-12-03 16:10:02 +0300

    Финиш

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <v-app>
+    <AppHeader />
+
     <v-main>
-      <AppHeader />
       <TasksBody />
     </v-main>
+
+    <v-footer app class="pa-4 justify-center text-caption">
+      © {{ new Date().getFullYear() }} ·
+      <a
+        href="https://t.me/antonGolova"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="ml-1 text-decoration-none"
+      >
+        @antonGolova
+      </a>
+    </v-footer>
   </v-app>
 </template>
 
@@ -13,4 +26,7 @@ import TasksBody from '@/components/TasksBody.vue'
 </script>
 
 <style scoped>
+a {
+  color: inherit;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,30 @@
 <template>
-  <v-app-bar color="white" density="compact">
-    <template v-slot:prepend>
+  <v-app-bar density="compact">
+    <template #prepend>
       <v-img
-      class="logo mr-4"
-      src="https://storage.yandexcloud.net/forlogo/logo.svg"
-      alt="Логотип"
-      contain
-    />
-  </template>
+        class="logo mr-4"
+        src="https://storage.yandexcloud.net/forlogo/logo.svg"
+        alt="Логотип"
+        contain
+      />
+    </template>
 
     <v-app-bar-title class="text-truncate">
       Управление задачами
     </v-app-bar-title>
+
+    <template #append>
+      <v-btn
+        icon
+        variant="text"
+        href="https://github.com/scoutrul/tt-koshel"
+        target="_blank"
+        rel="noopener noreferrer"
+        :aria-label="'Открыть репозиторий на GitHub'"
+      >
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </template>
   </v-app-bar>
 </template>
 
@@ -24,5 +37,4 @@
   height: 50px;
 }
 </style>
-
 
